d4c9fdeabfd6246d35feee62c6a3abf4e9f3f82c
swu.zzm <1620724912@qq.com>
2021-09-18 11:11:29 +0800

    1.Layout搭建

---
+++ app/src/main/java/com/swu/zzm/android2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.swu.zzm.android2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+    
+}
